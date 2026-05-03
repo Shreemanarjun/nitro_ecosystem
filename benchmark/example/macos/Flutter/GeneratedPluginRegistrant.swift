d2c23bd3b7a38a5141fcc3bdc546acf562a63edf
Shreemanarjun <Shreemanarjunsahu@gmail.com>
2026-05-04 01:21:59 +0530

    fix

--- benchmark/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ benchmark/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import benchmark
-import nitro
 import package_info_plus
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BenchmarkPlugin.register(with: registry.registrar(forPlugin: "BenchmarkPlugin"))
-  NitroPlugin.register(with: registry.registrar(forPlugin: "NitroPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
